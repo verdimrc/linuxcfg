f0697773d378e61a11b83ce8e784ae0807b488f3
Verdi March <verdi.march@gmail.com>
2018-05-17 01:14:34 +0800

    Update jupyter_notebook_config.py

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -1,4 +1,4 @@
 # Show only modified options
 
 c.NotebookApp.browser = u'chromium-browser'
-c.NotebookApp.terminado_settings = { "shell_command": "/usr/bin/env bash" }
+#c.NotebookApp.terminado_settings = { "shell_command": ["/usr/bin/env", "bash"] }
