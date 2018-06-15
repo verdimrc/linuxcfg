33d8f39f169feee78b29c49a4bce586a7b0353be
Verdi March <verdi.march@gmail.com>
2018-06-15 14:04:34 +0800

    Update rsync.sh

--- doc/rsync.sh
+++ doc/rsync.sh
@@ -10,3 +10,10 @@ rsync -av --delete project user@1.2.3.4:/home/user/src
 rsync -av --delete user@1.2.3.4:/home/user/src/project .
 
 cd -
+
+######
+# Sync specific files.
+# NOTE: rsync_include.txt is a list of files, preferrably sorted to improve performance
+
+# remote -> local for specific files
+rsync -av --files-from=rsync_include.txt "$REMOTE" .
