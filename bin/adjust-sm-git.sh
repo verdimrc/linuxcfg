d4d9a9b3812ce08e78bdb5b11f192bf619b68e29
Verdi March <verdi.march@gmail.com>
2019-04-11 14:13:10 +0800

    adjust-sm-git.sh: set editor to vim

--- bin/adjust-sm-git.sh
+++ bin/adjust-sm-git.sh
@@ -12,6 +12,9 @@ git config --global user.email $USER_EMAIL
 echo You may need to run:
 echo '    ' git commit --amend --reset-author
 
+echo Set editor to /usr/bin/vim (for DL AMI)
+git config --global core.editor /usr/bin/vim
+
 echo Adjusting log aliases...
 git config --global alias.lol "log --graph --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(bold white)— %an%C(reset)%C(bold yellow)%d%C(reset)' --abbrev-commit --date=relative"
 git config --global alias.lola "log --graph --all --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(bold white)— %an%C(reset)%C(bold yellow)%d%C(reset)' --abbrev-commit --date=relative"
