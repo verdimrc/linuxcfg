6610b2239455e5f3aca7195b3fe43ef030e40f74
Verdi March <verdi.march@gmail.com>
2019-11-27 22:38:41 +0800

    Create update-ubuntu.sh

---
+++ bin/update-ubuntu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt update && sudo apt dist-upgrade -V -y
+sudo apt clean
+
+# miniconda
+declare -a conda_env=( $(for i in ~/miniconda3/envs/*; do [[ -d $i && ! $i =~ '^.' ]] && echo $(basename $i);done) )
+for i in base "${conda_env[@]}"; do
+    echo Updating conda environment: $i
+    ~/miniconda3/bin/conda update -n $i --all -y
+done
+~/miniconda3/bin/conda clean --all -y
+~/miniconda3/bin/conda build purge
