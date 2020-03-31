0e2d738dc7946de54a775ce6d0f1bc5c3ce87215
Verdi March <verdi.march@gmail.com>
2020-03-31 18:11:11 +0800

    Update git-cross-repo_branch.sh

--- doc/git-cross-repo_branch.sh
+++ doc/git-cross-repo_branch.sh
@@ -9,18 +9,21 @@ git commit
 git remote add origin <url-to-origin>
 git push --set-upstream origin master
 
-# Now it's time to merge commits from another_repo/branch_x
-# See also: https://stackoverflow.com/a/49095833
+# Add the other repo as a new remote.
 git checkout -b <my-branch>
 git remote add <the-other-repo-name> <url-of-the-other-repo>
 git remote update
 
+# Now it's time to merge commits from another_repo/branch_x
+# See also: https://stackoverflow.com/a/49095833
+#
 # If you are sure you want to accept all remote changes and
 # avoid conflicts (overwrite yours) then you can specify
 # -X theirs as option for git merge.
 git merge --allow-unrelated-histories <the-other-repo-name>/<their-branch>
 
+# Push merged to my origin.
 git push --set-upstream origin <my-branch>
 
-# Optional (which I actually did): remove oldrepo remote
+# Optional (which I actually did): remove the-other-repo-name remote
 git remote rm <the-other-repo-name>
