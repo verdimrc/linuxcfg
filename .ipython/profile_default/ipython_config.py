98ba29dfa2d251d9d2596c51ce2b5705ac9acd34
Verdi March <marcverd@amazon.com>
2020-10-17 19:19:21 +0800

    ipython_config.py: updated for newer ipython and/or pygments

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -1,10 +1,11 @@
 # See: https://stackoverflow.com/a/48455387
-from pygments.token import Token, Keyword, Name, Comment, String, Error, Number, Operator, Generic, Whitespace
+from pygments.token import Name
 
 # Change default dark blue for "object.__file__" to a more readable color.
 # Recommended for dark background.
 c.TerminalInteractiveShell.highlighting_style_overrides = {
-    Name.Variable: "#B8860B",
+    Name.Variable: "#B8860B",         # Older ipython or pygments (unclear since which versions)
+    Name.Variable.Magic: "#B8860B",   # Newer ipython or pygments (unclear since which versions)
     # Name.Variable: "#2CB5E9"
 }
 
