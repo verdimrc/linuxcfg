7c006d82166c849689a2b559a07a588554df77e8
Verdi March <marcverd@amazon.com>
2020-10-17 19:25:16 +0800

    ipython_config.py: documented how to probe pygments

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -1,8 +1,18 @@
 # See: https://stackoverflow.com/a/48455387
 from pygments.token import Name
 
-# Change default dark blue for "object.__file__" to a more readable color.
-# Recommended for dark background.
+"""
+Change default dark blue for "object.__file__" to a more readable color, esp. on dark background.
+
+Find out the correct token type with:
+
+>>> from pygments.lexers import PythonLexer
+>>> list(PythonLexer().get_tokens('os.__class__'))
+[(Token.Name, 'os'),
+ (Token.Operator, '.'),
+ (Token.Name.Variable.Magic, '__class__'),
+ (Token.Text, '\n')]
+"""
 c.TerminalInteractiveShell.highlighting_style_overrides = {
     Name.Variable: "#B8860B",         # Older ipython or pygments (unclear since which versions)
     Name.Variable.Magic: "#B8860B",   # Newer ipython or pygments (unclear since which versions)
@@ -10,4 +20,3 @@ c.TerminalInteractiveShell.highlighting_style_overrides = {
 }
 
 c.TerminalInteractiveShell.highlight_matching_brackets = True
-
