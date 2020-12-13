dd5ca61ff0b3e2bcae06275cdf9d32f5d30fc634
Verdi March <verdimrc@users.noreply.github.com>
2020-12-13 15:47:52 +0800

    Updated jupyter config

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -1,4 +1,11 @@
 # Show only modified options
 
-c.NotebookApp.browser = u'chromium-browser'
+#c.NotebookApp.browser = 'chromium-browser'
 #c.NotebookApp.terminado_settings = { "shell_command": ["/usr/bin/env", "bash"] }
+c.NotebookApp.open_browser = False
+c.NotebookApp.port_retries = 0
+c.KernelSpecManager.ensure_native_kernel = False
+
+# Needs: pip install environment_kernels
+c.NotebookApp.kernel_spec_manager_class = 'environment_kernels.EnvironmentKernelSpecManager'
+c.EnvironmentKernelSpecManager.virtualenv_env_dirs = ['/home/verdi/.pyenv/versions']
