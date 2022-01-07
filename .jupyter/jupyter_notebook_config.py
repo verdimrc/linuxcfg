df1ca65fd87cf9d197ce8419af2d3203a8926d13
Verdi March <verdimrc@users.noreply.github.com>
2022-01-08 01:06:02 +0800

    Jupyter: centralized .ipynb_checkpoints; no trash

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -8,4 +8,8 @@ c.KernelSpecManager.ensure_native_kernel = False
 
 # Needs: pip install environment_kernels
 c.NotebookApp.kernel_spec_manager_class = 'environment_kernels.EnvironmentKernelSpecManager'
+c.EnvironmentKernelSpecManager.find_conda_envs = False
 c.EnvironmentKernelSpecManager.virtualenv_env_dirs = ['/home/verdi/.pyenv/versions']
+
+c.FileCheckpoints.checkpoint_dir = '/tmp/.ipynb_checkpoints'
+c.FileContentsManager.delete_to_trash = False
