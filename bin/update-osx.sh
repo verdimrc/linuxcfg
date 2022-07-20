b81492bdcbbf588b9a1a470c28f523577746375c
Verdi March <verdimrc@users.noreply.github.com>
2022-07-20 12:56:23 +0800

    update-osx: print name of conda environment being updated

--- bin/update-osx.sh
+++ bin/update-osx.sh
@@ -15,8 +15,8 @@ echo 'Updating pipx packages...'
 pipx upgrade-all
 
 # updating conda
-echo 'Updating conda environments...'
 for i in base ~/.pyenv/versions/miniforge3/envs/base-*; do
+    echo "Updating conda environments $(basename $i)..."
     ~/.pyenv/versions/miniforge3/bin/conda update --all --yes -n `basename $i`
 done
 ~/.pyenv/versions/miniforge3/bin/conda clean --all --yes
