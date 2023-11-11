1f63a24447d266f3ae1942f81d0fec408b57a11b
Verdi March <marcverd@amazon.com>
2023-11-11 14:23:59 +0800

    Add archlinux util scripts

---
+++ archlinux/fix-pyenv.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+cat << 'EOF' > /usr/share/zsh/site-functions/_pyenv
+#compdef pyenv
+if [[ ! -o interactive ]]; then
+    return
+fi
+
+local state line
+typeset -A opt_args
+
+_arguments -C \
+    {--help,-h}'[Show help]' \
+    {--version,-v}'[Show pyenv version]' \
+    '(-): :->command' \
+    '*:: :->option-or-argument'
+
+case "$state" in
+    (command)
+        local -a commands
+        commands=(${(f)"$(pyenv commands)"})
+        _describe -t commands 'command' commands
+        ;;
+    (option-or-argument)
+        local -a args
+        args=(${(f)"$(pyenv completions ${line[1]})"})
+        _describe -t args 'arg' args
+        ;;
+esac
+
+return
+EOF
+
+rm /home/verdi/.zcompdump
