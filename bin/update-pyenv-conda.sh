7261210fc965a8aa7b5646ea478ee731a1f24c11
Verdi March <verdimrc@users.noreply.github.com>
2024-11-09 21:15:24 +0800

    update pyenv miniforge3-latest

---
+++ bin/update-pyenv-conda.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# updating pyenv
+echo 'Running pyenv rehash...'
+pyenv rehash
+
+# updating pipx
+echo 'Updating pipx packages...'
+pipx upgrade-all
+
+# Updating conda's python under pyenv. Special case for base python.
+echo "Updating conda's base python..."
+~/.pyenv/versions/miniforge3-latest/bin/conda update --yes --update-all -n base python
+
+# Updating all conda's environment.
+for i in base ~/.pyenv/versions/miniforge3-latest/envs/base-*; do
+    echo "Updating conda environments $(basename $i)..."
+    ~/.pyenv/versions/miniforge3-latest/bin/conda update --all --yes -n `basename $i`
+done
+~/.pyenv/versions/miniforge3-latest/bin/conda clean --all --yes
+
+## No longer uses miniconda. Left here for historical context only.
+##
+#declare -a conda_env=( $(for i in ~/miniconda3/envs/*; do [[ -d $i && ! $i =~ '^.' ]] && echo $(basename $i);done) )
+#for i in base "${conda_env[@]}"; do
+#    echo Updating conda environment: $i
+#    ~/miniconda3/bin/conda update -n $i --all -y
+#done
+#~/miniconda3/bin/conda clean --all -y
+#~/miniconda3/bin/conda build purge
