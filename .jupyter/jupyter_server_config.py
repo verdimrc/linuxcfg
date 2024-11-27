0e6d5073c2daf8c34975ada42a706412769688f7
Verdi March <verdimrc@users.noreply.github.com>
2024-11-27 17:24:44 +0800

    jlab: do not crash when environment_kernels not installed

--- .jupyter/jupyter_server_config.py
+++ .jupyter/jupyter_server_config.py
@@ -7,15 +7,19 @@ c.ServerApp.port_retries = 0
 c.KernelSpecManager.ensure_native_kernel = False
 c.LabServerApp.notebook_starts_kernel = False
 
-# Needs: pip install environment_kernels
-c.ServerApp.kernel_spec_manager_class = 'environment_kernels.EnvironmentKernelSpecManager'
-c.EnvironmentKernelSpecManager.find_conda_envs = False
-c.EnvironmentKernelSpecManager.use_conda_directly = False
-c.EnvironmentKernelSpecManager.blacklist_envs = ['virtualenv_jlab']
-import os.path
-c.EnvironmentKernelSpecManager.virtualenv_env_dirs = [os.path.expanduser('~/.pyenv/versions')]
-#c.EnvironmentKernelSpecManager.find_conda_envs = True
-#c.EnvironmentKernelSpecManager.conda_env_dirs = [os.path.expanduser('~/.pyenv/versions/miniforge3-latest/envs')]
+try:
+    import environment_kernels
+except:
+    pass
+else:
+    # Needs: pip install environment_kernels (within the jlab env)
+    c.ServerApp.kernel_spec_manager_class = 'environment_kernels.EnvironmentKernelSpecManager'
+    c.EnvironmentKernelSpecManager.find_conda_envs = False   # Change to True as needed
+    c.EnvironmentKernelSpecManager.use_conda_directly = False
+    c.EnvironmentKernelSpecManager.blacklist_envs = ['virtualenv_jlab']
+    import os.path
+    c.EnvironmentKernelSpecManager.virtualenv_env_dirs = [os.path.expanduser('~/.pyenv/versions')]
+    c.EnvironmentKernelSpecManager.conda_env_dirs = [os.path.expanduser('~/.pyenv/versions/miniforge3-latest/envs')]
 
 c.FileCheckpoints.checkpoint_dir = '/tmp/.ipynb_checkpoints'
 c.FileContentsManager.delete_to_trash = False
