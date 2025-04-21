a333ce9c0cf93a9c24606c0a7a204f7c2cf146cf
Verdi March <verdimrc@users.noreply.github.com>
2025-04-21 11:13:11 +0800

    Outlook classic: cli to toggle online/offline startup mode

---
+++ windows/outlook-classic-online.bat
@@ -0,0 +1,3 @@
+:: https://mikecrowley.us/2018/07/05/start-outlook-in-offline-mode-without-opening-it-first/
+
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Office\16.0\Outlook\Profiles\Outlook\0a0d020000000000c000000000000046" /t REG_BINARY /v "00030398" /d "02000000" /f
