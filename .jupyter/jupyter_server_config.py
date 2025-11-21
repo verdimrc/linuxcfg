9cb16ed57b5a7e8733d1d8d3bebe58170c9a481b
Verdi March <verdimrc@users.noreply.github.com>
2025-11-22 02:11:16 +0800

    jlab: customize layout to retain

--- .jupyter/jupyter_server_config.py
+++ .jupyter/jupyter_server_config.py
@@ -1,5 +1,3 @@
-# https://jupyter-server.readthedocs.io/en/stable/operators/migrate-from-nbserver.html
-
 #c.ServerApp.browser = 'chromium-browser'
 #c.ServerApp.terminado_settings = { "shell_command": ["/usr/bin/env", "bash"] }
 c.ServerApp.open_browser = False
@@ -24,3 +22,67 @@ else:
 c.FileCheckpoints.checkpoint_dir = '/tmp/.ipynb_checkpoints'
 c.FileContentsManager.delete_to_trash = False
 c.FileContentsManager.always_delete_dir = True
+
+# Retain layout, but do not reopen files, terminals, consoles.
+def __haha():
+    '''Use an unlikely function name.
+
+    ---
+    # YAML fragment of elements to drop
+    data:
+      file-browser-filebrowser:openState    # ALL GONE
+      layout-restorer:data
+        main              # ALL GONE
+      docmanager:recents  # ALL GONE
+      console:asdf        # ALL GONE
+      editor:asdf         # ALL GONE
+      notebook:asdf       # ALL GONE
+      terminal:asdf       # ALL GONE
+    ---
+    '''
+    import glob
+    import os
+
+    def update_workspace(fname):
+        import json
+
+        with open(fname, 'r') as f:
+            d = json.load(f)
+
+        try: del d['data']['layout-restorer:data']['main']
+        except Exception as e: print(e)
+
+        for k in list(d['data'].keys()):
+            if (k.startswith('docmanager:recents')
+                or k.startswith('console:')
+                or k.startswith('editor:')
+                or k.startswith('notebook:')
+                or k.startswith('terminal:')
+                or k.startswith('file-browser-filebrowser:openState')
+            ):
+                try: del d['data'][k]
+                except Exception as e: print(e)
+
+        with open(fname, 'w') as f:
+            json.dump(d, f)
+
+    # Scan hardcoded path because c.LabApp.workspace_dir = LazyConfigXxx('')
+    workspaces = glob.glob(os.path.expanduser('~/.jupyter/lab/workspaces/default-*.jupyterlab-workspace'))
+    for fname in workspaces:
+        update_workspace(fname)
+
+__haha()
+del __haha
+
+
+###############################################################################
+# Disabling workspace: other approaches tried
+###############################################################################
+# Official way is ineffective. Terminals still reopen.
+#c.ServerApp.default_url = '/lab?reset'
+
+# This nukes everything, but I still like to retain some layout
+# https://github.com/jupyterlab/jupyterlab/issues/7330#issuecomment-2540017454
+#import tempfile
+#c.LabApp.workspaces_dir = tempfile.mkdtemp()
+###############################################################################
